730cb6e4dba8e9ac557961d11267f3298ae3cb51
Rainspistols <ffly@mail.ru>
2020-04-26 00:32:59 +0200

    Updates

---
+++ precache-manifest.7c8c1fb832ebd52708de3d05e9db1329.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74392c8ce5f20ec8e06d258681c5ad2a",
+    "url": "/React-myPortfolio2020/index.html"
+  },
+  {
+    "revision": "5c3cfc8d48cfff4c8c73",
+    "url": "/React-myPortfolio2020/static/css/main.b9c27c7c.chunk.css"
+  },
+  {
+    "revision": "c30b78e2b9717ef2d51e",
+    "url": "/React-myPortfolio2020/static/js/2.eacae715.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/React-myPortfolio2020/static/js/2.eacae715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3cfc8d48cfff4c8c73",
+    "url": "/React-myPortfolio2020/static/js/main.327c86e2.chunk.js"
+  },
+  {
+    "revision": "b4a21d18b6b41a7477ab",
+    "url": "/React-myPortfolio2020/static/js/runtime-main.86dfd2ab.js"
+  },
+  {
+    "revision": "3eb5699716d7d1ac3a12af382696411b",
+    "url": "/React-myPortfolio2020/static/media/BLAH@2x.3eb56997.jpg"
+  },
+  {
+    "revision": "8f024e5e9a89b66a5c1e45ed76ad9f7a",
+    "url": "/React-myPortfolio2020/static/media/barbershopBor.8f024e5e.jpg"
+  },
+  {
+    "revision": "904e7b8e5961fb51fd1702fe944e2bc9",
+    "url": "/React-myPortfolio2020/static/media/cakeBooking.904e7b8e.jpg"
+  },
+  {
+    "revision": "df96a64c6203e7ec3a8ca5be0067f88d",
+    "url": "/React-myPortfolio2020/static/media/catEnergy.df96a64c.jpg"
+  },
+  {
+    "revision": "d11324abdcdd37adb235a1b9d72ba4c6",
+    "url": "/React-myPortfolio2020/static/media/codeAndMagic.d11324ab.jpg"
+  },
+  {
+    "revision": "cdf5933907ee09c31993d9045fa8f0aa",
+    "url": "/React-myPortfolio2020/static/media/deviceShop.cdf59339.jpg"
+  },
+  {
+    "revision": "fd1247450c9d9bf14c1289363f18d6ca",
+    "url": "/React-myPortfolio2020/static/media/goSurf.fd124745.jpg"
+  },
+  {
+    "revision": "7629142def4488b5eac9bd57091160cc",
+    "url": "/React-myPortfolio2020/static/media/reactHooks.7629142d.jpg"
+  },
+  {
+    "revision": "d62fb9e0ddd0803eba69ed1fbd760de2",
+    "url": "/React-myPortfolio2020/static/media/reactQuiz.d62fb9e0.jpg"
+  },
+  {
+    "revision": "c845c9bdbfd04e1d79ccb7b70e19634c",
+    "url": "/React-myPortfolio2020/static/media/sedona.c845c9bd.jpg"
+  }
+]);
